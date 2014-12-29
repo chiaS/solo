6ad25688c9696dfd40d79b19fa4b328c868408d0
chiaS <pretend15@gmail.com>
2014-12-28 22:06:41 -0800

    move server.js one level up

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
-var db = require('./db');
-var router = require('./routes');
+var db = require('./server/db');
+var router = require('./server/routes');
 var fs = require('fs');
 var request = require('request');
 
@@ -15,10 +15,10 @@ app.use(function(req, res, next) {
   next();
 });
 app.use("/stories", router);
-app.use(express.static(path.join(__dirname, '/../client')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/stories', function (req, res) {
-  res.sendfile(path.join(__dirname, '/../client/index.html'));
+  res.sendfile(path.join(__dirname, 'client/index.html'));
 });
 
 
